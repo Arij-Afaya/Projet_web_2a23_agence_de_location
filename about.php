81c76709b40e348549ef4905e72701e3032788ab
sedraouii <103000058+sedraouii@users.noreply.github.com>
2022-05-10 09:23:40 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- stella-orre/  30 Nov 2019 03:42:43 GMT -->
+<head>
+<meta charset="utf-8">
+<title>Localuxe </title>
+<!-- Stylesheets -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+
+<div class="page-wrapper">
+    <!-- Preloader -->
+    <div class="preloader"></div>
+
+    <header class="main-header header-style-one">
+      
+
+        <!-- Header Upper -->
+        <div class="header-upper">
+            <div class="inner-container">
+                <div class="auto-container clearfix">
+                    <!--Info-->
+                    <div class="logo-outer">
+                        <div class="logo"><a href="index-2.html"><img src="images/localuxe.png" alt="" title=""></a></div>
+                    </div>
+
+                    <!--Nav Box-->
+                    <div class="nav-outer clearfix">
+                        <!--Mobile Navigation Toggler For Mobile--><div class="mobile-nav-toggler"><span class="icon flaticon-menu-1"></span></div>
+                        <nav class="main-menu navbar-expand-md navbar-light">
+                            <div class="navbar-header">
+                                <!-- Togg le Button -->      
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="icon flaticon-menu-1"></span>
+                                </button>
+                            </div>
+                            
+                            <div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
+                                <ul class="navigation clearfix">
+                                    <li class="dropdown"><a href="index-2.php">Acceuil</a>
+                                        
+                                    </li>
+									<li class="dropdown"><a href="about.html">A propos de nous</a>
+                                        <ul>
+                                            <li><a href="about.php">Notre equipe</a></li>
+										
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown"><a href="affichagev.php">Vehicules</a>
+                                        <ul>
+                                            <li><a href="affichagev.php">Liste véhicules</a></li>
+                                            <li><a href="searchfront.php">Liste Disponibilité</a></li>
+                                            
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown"><a href="#">Evenement</a>
+                                        <ul>
+                                            <li><a href="projects-classic-nourhene.php">nos evenements</a></li>
+                                            <li><a href="projects-fullwidth-nourhene.php">nos sponsors</a></li>
+                                            <!--<li><a href="blog-detail.html">Blog Details</a></li>-->
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown"><a href="services-dark.html">Contacts</a>
+                                        
+                                    </li>
+                                    <li class="dropdown"><a href="reclamation.php">Reclamations</a>
+                                        
+                                    </li>
+									
+                                    <li class="dropdown"><a href="log.php">Login</a></li>
+                                      
+                                </ul>
+                            </div>
+                        </nav>
+                        <!-- Main Menu End-->
+						
+						<!-- Outer Box -->
+                        <div class="outer-box clearfix">
+                            <div class="search-box-btn"><span class="icon flaticon-magnifying-glass-1"></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--End Header Upper-->
+
+    	
+
+    </header>
+    <!-- End Main Header -->
+
+    <!--Page Title-->
+    <section class="page-title" style="background-image:url(images/background/1.png)">
+    	<div class="auto-container">
+        	<h2>Notre Equipe</h2>
+            <ul class="page-breadcrumb">
+            	<li><a href="index-2.html">Accueil</a></li>
+                <li>Notre Equipe</li>
+            </ul>
+        </div>
+    </section>
+    <!--End Page Title-->
+	
+	<!-- Team Page Section -->
+	<section class="team-page-section">
+		<div class="auto-container">
+			
+			
+					
+					
+					
+				
+			<!-- Sec Title -->
+			<div class="sec-title">
+				<h2>L'Equipe de Lucaluxe</h2>
+				
+			
+			
+			<div class="clearfix">
+				
+				 
+<!-- Team Block -->
+<div class="team-block col-lg-3 col-md-6 col-sm-12">
+	<div class="inner-box">
+		<div class="image">
+			<img src="images/resource/d15.jpg" alt="" />
+			<div class="overlay-box">
+				<ul class="social-icons">
+					<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+					<li><a href="#"><i class="fab fa-skype"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="lower-content">
+			<h3><a href="team.html">Ben Romdhane Wajdi</a></h3>
+			<div class="designation">Gestion de Réclamation</div>
+		</div>
+	</div>
+</div>
+
+<!-- Team Block -->
+<div class="team-block col-lg-3 col-md-6 col-sm-12">
+	<div class="inner-box">
+		<div class="image">
+			<img src="images/resource/q1.jpg" alt="" />
+			<div class="overlay-box">
+				<ul class="social-icons">
+					<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+					<li><a href="#"><i class="fab fa-skype"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="lower-content">
+			<h3><a href="team.html">Chammakhi Nourhene</a></h3>
+			<div class="designation">Gestion des événements</div>
+		</div>
+	</div>
+</div>
+
+<!-- Team Block -->
+<div class="team-block col-lg-3 col-md-6 col-sm-12">
+	<div class="inner-box">
+		<div class="image">
+			<img src="images/resource/q2.jpg" alt="" />
+			<div class="overlay-box">
+				<ul class="social-icons">
+					<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+					<li><a href="#"><i class="fab fa-skype"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="lower-content">
+			<h3><a href="team.html">Ben Hamida Aziz</a></h3>
+			<div class="designation">Gestion de facture</div>
+		</div>
+	</div>
+</div>
+
+<!-- Team Block -->
+<div class="team-block col-lg-3 col-md-6 col-sm-12">
+	<div class="inner-box">
+		<div class="image">
+			<img src="images/resource/q3.jpg" alt="" />
+			<div class="overlay-box">
+				<ul class="social-icons">
+					<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+					<li><a href="#"><i class="fab fa-skype"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="lower-content">
+			<h3><a href="team.html">Sadraoui Samar</a></h3>
+			<div class="designation">Gestion des utilisateurs</div>
+		</div>
+	</div>
+</div>
+
+<!-- Team Block -->
+<div class="team-block col-lg-3 col-md-6 col-sm-12">
+	<div class="inner-box">
+		<div class="image">
+			<img src="images/resource/q4.jpg" alt="" />
+			<div class="overlay-box">
+				<ul class="social-icons">
+					<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+					<li><a href="#"><i class="fab fa-skype"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="lower-content">
+			<h3><a href="team.html">Afaya Arij</a></h3>
+			<div class="designation">Gestion des vehicules</div>
+		</div>
+	</div>
+</div>
+
+	</section>
+	<!-- End Story Section -->
+	
+	<!--Main Footer-->
+    <footer class="main-footer">
+		<div class="auto-container">
+        	<!--Widgets Section-->
+            <div class="widgets-section">
+            	<div class="row clearfix">
+                	
+                    <!--big column-->
+                    <div class="big-column col-lg-6 col-md-12 col-sm-12">
+                        <div class="row clearfix">
+                        
+                            <!--Footer Column-->
+                            <div class="footer-column col-lg-7 col-md-6 col-sm-12">
+                                <div class="footer-widget logo-widget">
+									<div class="logo">
+                                    	<a href="index-2.html"><img src="images/localuxe.png" alt="" /></a>
+                                    </div>
+                                    
+                                    <ul class="social-icons">
+                                        <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+                                        <li><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
+                                        <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                                        <li><a href="#"><span class="fab fa-google-plus-g"></span></a></li>
+                                    </ul>
+								</div>
+							</div>
+							
+							<!--Footer Column-->
+                            <div class="footer-column col-lg-5 col-md-6 col-sm-12">
+                                <div class="footer-widget links-widget">
+                                	
+									
+								</div>
+							</div>
+						
+						</div>
+					</div>
+					
+					<!--big column-->
+                    <div class="big-column col-lg-6 col-md-12 col-sm-12">
+                        <div class="row clearfix">
+                        
+                            <!--Footer Column-->
+                            <div class="footer-column col-lg-5 col-md-6 col-sm-12">
+                                <div class="footer-widget contact-widget">
+									<h2>Nos Contacts</h2>
+									<div class="widget-content">
+										<a href="tel:1800-574-9687" class="contact-number">(+216)94309914</a>
+										<ul>
+											<li>18, rue de l'Usine - ZI Aéroport Charguia II </li>
+											<li>Email :<a href="mailto:info@stellaorre.com"> info@stellaorre.com</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+							<!--Footer Column-->
+                            <div class="footer-column col-lg-7 col-md-6 col-sm-12">
+                                <div class="footer-widget newsletter-widget">
+                                	<h2>Contactez Nous</h2>
+									<div class="text">Profitez de nos offres</div>
+									<!-- Newsletter Form -->
+									<div class="newsletter-form">
+										<form method="post" action="templateshub.net">
+											<div class="form-group">
+												<input type="email" name="email" value="" placeholder="Entrer votre Adresse Email" required>
+												<button type="submit" class="theme-btn btn-style-one"><span class="txt">S'inscrire</span></button>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			
+			<!--Footer Bottom-->
+            <div class="footer-bottom clearfix">
+                <div class="pull-left">
+                    <div class="copyright"><a href="templateshub.net">Templates Hub</a></div>
+                </div>
+                <div class="pull-right">
+                    <a href="templateshub.net">Templates Hub</a>
+                </div>
+            </div>
+			
+		</div>
+	</footer>
+	
+</div>  
+<!--End pagewrapper-->
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-up"></span></div>
+
+<!--Search Popup-->
+<div id="search-popup" class="search-popup">
+	<div class="close-search theme-btn"><span class="flaticon-cancel"></span></div>
+	<div class="popup-inner">
+		<div class="overlay-layer"></div>
+    	<div class="search-form">
+        	<form method="post" action="templateshub.net">
+            	<div class="form-group">
+                	<fieldset>
+                        <input type="search" class="form-control" name="search-input" value="" placeholder="Search Here" required >
+                        <input type="submit" value="Search Now!" class="theme-btn">
+                    </fieldset>
+                </div>
+            </form>
+            
+            <br>
+            <h3>Recent Search Keywords</h3>
+            <ul class="recent-searches">
+                <li><a href="#">Home Interiors</a></li>
+                <li><a href="#">Offices Interiors</a></li>
+                <li><a href="#">Showroom Interiors</a></li>
+                <li><a href="#">Building Interiors</a></li>
+                <li><a href="#">Shops Interiors</a></li>
+            </ul>
+        
+        </div>
+        
+    </div>
+</div>
+	
+
+<!--Scroll to top-->
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/scrollbar.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+<!-- stella-orre/  30 Nov 2019 03:45:45 GMT -->
+</html>
